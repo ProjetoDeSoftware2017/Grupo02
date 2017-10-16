2d4548ff50b6470ada59381c134ea92d38118bc8
Claudiomiro diaz <juniordiaz489@hotmail.com>
2017-10-15 22:57:50 -0200

    Icones e localizacao na combo box

---
+++ ProjetoSoftware/ProjetoSoftware/Controllers/LocalizacaoController.cs
@@ -0,0 +1,32 @@
+﻿using ProjetoSoftware.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoSoftware.Controllers
+{
+    public class LocalizacaoController : Controller
+    {
+        private Contexto db = new Contexto();
+        // GET: Localizacao
+        public ActionResult Index()
+        {
+            ViewBag.ListarDropdown = db.Naufragos.Distinct().ToList();
+            return View();
+        }
+
+        public JsonResult PegaLocalizacao()
+        {
+            var data = db.Naufragos.ToList();
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult PegarLocalizcaoPorId(int id)
+        {
+            var data = db.Naufragos.Where(x => x.IdNaufrago == id).SingleOrDefault();
+            return Json(data,JsonRequestBehavior.AllowGet);
+        }
+    }
+}
